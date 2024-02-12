e79c77e4e1a70e2936166ebc18b23d56cb5c3a32
AbYahaya <yabdulrauf911@gmail.com>
2024-02-12 11:46:22 -0800

    State

---
+++ models/state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Another model that will inherit from BaseModel
+Created on 2/12/2024 by Yahaya Abdulrauf
+"""
+
+class State(BasModels):
+    name = ""
+
+    def __int__(self, *args, **kwargs):
+        """
+        The init method of the State class
+        It has:
+        Args: A tuple of arguments
+        Kwargs: A dict of arguments to the function
+        """
+
+        super().__init__(*args, **kwargs)
