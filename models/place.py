8cd71303ab2864be9de1faf4d26d132799a34b86
AbYahaya <yabdulrauf911@gmail.com>
2024-02-13 02:19:52 -0800

    place

---
+++ models/place.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Created by Yahaya Abdulrauf
+2/13/2024
+"""
+
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+    """
+    This class inherits from BaseModel
+    """
+    def __init__(self, *args, **Kwargs):
+        """
+        The init method of this class
+        """
+        super().__init__(*args, **kwargs)
